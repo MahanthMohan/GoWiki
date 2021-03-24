1d10bae7455e233af35cf791d3224f8c8ee7cfea
Mahanth <mohan.mahanth@gmail.com>
2021-03-24 16:20:22 -0700

    changes to backend

--- server/go.mod
+++ server/go.mod
@@ -1,10 +1,5 @@
 module example.com/gowiki
 
-// +heroku goVersion go	1.15
 go 1.15
 
-require (
-	github.com/gorilla/mux v1.8.0
-	go.mongodb.org/mongo-driver v1.4.2
-	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e
-)
+require go.mongodb.org/mongo-driver v1.5.0
--- server/main.go
+++ server/main.go
@@ -4,48 +4,34 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"time"
-
-	"github.com/gorilla/mux"
-	"golang.org/x/time/rate"
+	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// mongo model for the article struct
 type article struct {
-	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Title  string             `json:"title,omitempty"`
-	Body   string             `json:"body,omitempty"`
-	Image  string             `json:"image,omitempty"`
-	Author string             `json:"author,omitempty"`
+	Title  string `json:"title,omitempty"`
+	Body   string `json:"body,omitempty"`
+	Image  string `json:"image,omitempty"`
+	Author string `json:"author,omitempty"`
 }
 
-// Global constants (Exception: credential field in databaseURI)
-var databaseURI = "mongodb+srv://mahanth:" + getCredential() + "@gowiki.ayckl.mongodb.net/GoWiki?retryWrites=true&w=majority"
+const (
+	myDatabase   = "GoWiki"
+	myCollection = "Articles"
+)
 
-const myDatabase = "GoWiki"
-const myCollection = "Articles"
+var (
+	databaseURI = "mongodb+srv://mahanth:" + os.Getenv("DB_SECRET") +
+		"@gowiki.ayckl.mongodb.net/GoWiki?retryWrites=true&w=majority"
+	collection *mongo.Collection
+)
 
-// Creates a global variable of type mongo.Collection to be used in functions
-var collection *mongo.Collection
-
-func getCredential() string {
-	res, err := ioutil.ReadFile("credentials.txt")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return string(res)
-}
-
-// A function to initialize database before running CRUD operations
 func init() {
 	clientOptions := options.Client().ApplyURI(databaseURI)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
@@ -53,34 +39,13 @@ func init() {
 		log.Fatal(err)
 	}
 	log.Println("** Database connection successful **")
-	// Assigns the value of the collection to var collection
 	collection = client.Database(myDatabase).Collection(myCollection)
 	log.Println("** Created a collection **")
 }
 
-// A limiter that has a ceiling of maximum of 60 requests per minute => 1 s/r
-// bucketSize = 3 requests -> queued
-var limiter = rate.NewLimiter(rate.Every(time.Second), 3)
-
-// A rate limit function to limit the number of requests per minute
-func rateLimit(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if limiter.Allow() == false {
-			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
-			return
-		}
-		h.ServeHTTP(w, r)
-	})
-}
-
-// Each http function has two input parameters a http.ResponseWriter and a Request, which is a struct object
-// Create an article
 func createArticle(w http.ResponseWriter, r *http.Request) {
-	// All the headers for a post request
-	w.Header().Set("Context-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	var newArticle article
 	_ = json.NewDecoder(r.Body).Decode(&newArticle)
 	insertResult, err := collection.InsertOne(context.Background(), newArticle)
@@ -88,65 +53,44 @@ func createArticle(w http.ResponseWriter, r *http.Request) {
 		log.Fatalln(err)
 	}
 	log.Println(insertResult)
-	json.NewEncoder(w).Encode(newArticle)
+	w.WriteHeader(200)
 }
 
-// Read all the articles
 func getAllArticles(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	mongoCursor, err := collection.Find(context.Background(), bson.M{})
 	if err != nil {
 		log.Fatalln(err)
 	}
-	var articles []primitive.M
-	// Next document in the mongo collection
+
+	var articles []article
 	for mongoCursor.Next(context.Background()) {
-		var document bson.M
+		var document article
 		mongoCursor.Decode(&document)
 		articles = append(articles, document)
 	}
 	mongoCursor.Close(context.Background())
-	// Encode the document to json and write it as a json response
 	json.NewEncoder(w).Encode(articles)
 }
 
-// Delete an article
 func deleteArticle(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	args := mux.Vars(r)
-	id, err := primitive.ObjectIDFromHex(args["id"])
-	if err != nil {
-		log.Fatalln("Error parsing the _id of document")
-	}
-	deleteResult, err := collection.DeleteOne(context.Background(), bson.M{"_id": id})
+	title := strings.Split(r.URL.Path, "/")[3]
+	deleteResult, err := collection.DeleteOne(context.Background(), bson.M{"title": title})
 	if err != nil {
 		log.Fatalln(err)
 	}
 	log.Println(deleteResult)
-	json.NewEncoder(w).Encode(args["id"])
-}
-
-// Router function to handle all http routes using a mux router
-func Router() *mux.Router {
-
-	router := mux.NewRouter()
-
-	router.HandleFunc("/api/create", createArticle).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/read", getAllArticles).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/delete/{id}", deleteArticle).Methods("DELETE", "OPTIONS")
-	return router
+	w.WriteHeader(200)
 }
 
 func main() {
-	r := Router()
 	fmt.Println("Server running on PORT 8080")
+	http.HandleFunc("/api/create", createArticle)
+	http.HandleFunc("/api/read", getAllArticles)
+	http.HandleFunc("/api/delete/", deleteArticle)
 	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	log.Fatal(http.ListenAndServe(":"+port, rateLimit(r)))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
